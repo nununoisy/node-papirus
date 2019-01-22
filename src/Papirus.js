d7d5201475d8b62c118007c399a609496e2d3761
nununoisy <nununoisy@gmail.com>
2019-01-22 11:29:03 -0700

    Add JSDoc documentation

--- src/Papirus.js
+++ src/Papirus.js
@@ -3,7 +3,18 @@ import Epd from './Epd';
 import EpdButtons from './EpdButtons';
 import PapirusImage from './PapirusImage';
 
+/**
+ * Class representing the interface to the Papirus.
+ * @class
+ */
 export default class Papirus {
+	/**
+	 * Creates a new Papirus interface.
+	 *
+	 * @param {Number} width - Width of the screen.
+	 * @param {Number} height - Height of the screen.
+	 * @param {Boolean} piZero - True for Papirus Zero, false otherwise. Used for mapping button events.
+	 */
 	constructor(width, height, piZero) {
 		this.width = width;
 		this.height = height;
@@ -12,13 +23,19 @@ export default class Papirus {
 		this.buttons = new EpdButtons(piZero);
 		this.image = new PapirusImage(this.width, this.height);
 	}
-
+	/**
+	 * Resets the EPD FUSE library.
+	 * @returns {Papirus}
+	 */
 	reset() {
 		return this.epd.clear().then(() => {
 			return this;
 		});
 	}
-
+	/**
+	 * Clears the Papirus screen buffer.
+	 * @returns {Papirus}
+	 */
 	clear() {
 		this.queue.push(() => {
 			this.image = new PapirusImage(this.width, this.height);
@@ -26,7 +43,15 @@ export default class Papirus {
 		});
 		return this;
 	}
-
+	/**
+	 * Draws an image to the screen buffer.
+	 *
+	 * @param {(string|Buffer)} path - Path to image or Buffer containing image data.
+	 * @param {Boolean} [dither=true] - Whether to use dithering during color to monochrome conversion.
+	 * @param {Number} [x=0] - X position of the image.
+	 * @param {Number} [y=0] - Y position of the image.
+	 * @returns {Papirus}
+	 */
 	addImage(path, dither = true, x = 0, y = 0) {
 		this.queue.push(() => {
 			console.log(`Opening image '${path}'...`);
@@ -41,7 +66,15 @@ export default class Papirus {
 			});
 		return this;
 	}
-
+	/**
+	 * Draws text to the screen buffer.
+	 *
+	 * @param {string} text - Text to draw.
+	 * @param {string} font - Path to a .fnt bitmap font file.
+	 * @param {Number} [x=0] - X position of text.
+	 * @param {Number} [y=0] - Y position of text.
+	 * @returns {Papirus}
+	 */
 	addText(text, font, x = 0, y = 0, id = null) {
 		console.log(`Writing "${text}" at (${x},${y})`);
 		this.queue.push(() => {
@@ -49,7 +82,12 @@ export default class Papirus {
 		});
 		return this;
 	}
-
+	/**
+	 * Write the screen buffer to the Papirus screen.
+	 *
+	 * @param {Boolean} [partial=false] - Whether to perform a partial (fast) or full (clean) update.
+	 * @returns {Boolean} - True on success.
+	 */
 	write(partial = false) {
 		this.queue.push(() => {
 			return this.image
@@ -69,7 +107,12 @@ export default class Papirus {
 		});
 		return this.done();
 	}
-
+	/**
+	 * Writes the screen buffer to an image file.
+	 *
+	 * @param {string} path - Path to write image file to.
+	 * @returns {Boolean} - True if success.
+	 */
 	writeToFile(path) {
 		this.queue.push(() => {
 			console.log(`Writing image to file ${path}...`);
@@ -84,17 +127,29 @@ export default class Papirus {
 		});
 		return this.done();
 	}
-
+	/**
+	 * Builds the screen buffer without writing it.
+	 * @returns {Papirus}
+	 */
 	done() {
 		return this.queue.execute().then(() => {
 			return this;
 		});
 	}
-
+	/**
+	 * Assigns an event handler to a button press event. Can be stacked.
+	 *
+	 * @param {Number} index - Zero-indexed button number.
+	 * @param {Papirus~buttonCallback} cb - Button event handler.
+	 */
 	onButton(index, cb) {
 		this.buttons.listen(index, cb);
 	}
-
+	/**
+	 * Callback for a button event.
+	 * @callback Papirus~buttonCallback
+	 * @param {Onoff~Gpio} button - Onoff GPIO object representing the GPIO attached to the button.
+	 */
 	static get FONT_SANS_8_BLACK() { return PapirusImage.FONT_SANS_8_BLACK; }
 	static get FONT_SANS_16_BLACK() { return PapirusImage.FONT_SANS_16_BLACK; }
 	static get FONT_SANS_32_BLACK() { return PapirusImage.FONT_SANS_32_BLACK; }
